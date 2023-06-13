1edb878782d7ce4cd0d6899ae60f269925f05f7a
josibertopedro <josibertopedro@edu.unifor.br>
2023-06-13 18:25:39 -0300

    modificaçoes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ async function connect() {
   if (global.db) return global.db
 
   const conn = await MongoClient.connect(
-    'mongodb+srv://adelesouza:Everaldo8@cluster0.vo4ob5s.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://pjosiberto95:josiberto@cluster0.ayuph9r.mongodb.net/?retryWrites=true&w=majority'
   )
   if (!conn) return new Error('ERRO de conexão!')
   global.db = await conn.db('unifor')
@@ -22,7 +22,7 @@ app.use(express.json())
 
 //api teste
 const router = express.Router()
-router.get('/', (req, res) => res.json({ message: 'Funcionou!' }))
+app.get('/', (req, res) => res.json({ message: 'Funcionou!' }))
 
 //GET
 router.get('/produto/:id?', async function (req, res, next) {
@@ -75,6 +75,24 @@ router.post('/produto', async function (req, res, next) {
   }
 })
 
+router.get('/undproduto/:id?', async function (req, res, next) {
+  //o "?" depois do :id diz que é um parâmetro opcional
+  try {
+    const db = await connect()
+    if (req.params.id) {
+      res.json(
+        await db
+          .collection('undproduto')
+          .findOne({ _id: new ObjectId(req.params.id) })
+      )
+    } else {
+      res.json(await db.collection('undproduto').find().toArray())
+    }
+  } catch (ex) {
+    console.log(ex)
+    res.status(400).json({ erro: `${ex}` })
+  }
+})
 router.post('/undproduto', async function (req, res, next) {
   try {
     const undproduto = req.body
@@ -145,7 +163,112 @@ router.delete('/undproduto/:id', async function (req, res, next) {
   }
 })
 
-app.use('/', router)
+//josiberto
+//POST
+app.post('/adm3', async function (req, res, next) {
+  try {
+    const usuario = req.body
+    const db = await connect()
+    console.log(usuario)
+    res.json(await db.collection('aluno').insertOne(usuario))
+  } catch (ex) {
+    console.log(ex)
+    res.status(400).json({ erro: `${ex}` })
+  }  
+})
+app.get('/adm3/:id', async function (req, res, next) {
+  //o "?" depois do :id diz que é um parâmetro opcional
+  try {
+    const db = await connect()
+    if (req.params.id) {
+      res.json(
+        await db
+          .collection('aluno')
+          .findOne({ _id: new ObjectId(req.params.id) })
+      )
+    } else {
+      res.json(await db.collection('aluno').find().toArray())
+    }
+  } catch (ex) {
+    console.log(ex)
+    res.status(400).json({ erro: `${ex}` })
+  }
+})
+
+router.get("/adm3", async function (req, res, next) {
+  //o "?" depois do :id diz que é um parâmetro opcional
+  try{
+    const db = await connect();
+    const collection = await db.collection('aluno');
+    const result = await collection.find().toArray();
+    console.log(result);
+    res.json(result);
+  }catch(err){
+    res.json(err);
+  }
+})
+
+
+//PUT
+app.put('/adm3/:id', async function (req, res, next) {
+  try {
+    const produto = req.body
+  
+    const db = await connect()
+    res.json(
+      await db
+        .collection('produto')
+        .updateOne({ _id: new ObjectId(req.params.id) }, { $set: produto })
+    )
+  } catch (ex) {
+    console.log(ex)
+    res.status(400).json({ erro: `${ex}` })
+  }
+})
+router.put('/undproduto/:id', async function (req, res, next) {
+  try {
+    const undproduto = req.body
+    const db = await connect()
+    res.json(
+      await db
+        .collection('undproduto')
+        .updateOne({ _id: new ObjectId(req.params.id) }, { $set: undproduto })
+    )
+  } catch (ex) {
+    console.log(ex)
+    res.status(400).json({ erro: `${ex}` })
+  }
+})
+
+//DELETE
+router.delete('/produto/:id', async function (req, res, next) {
+  try {
+    const db = await connect()
+    res.json(
+      await db
+        .collection('produto')
+        .deleteOne({ _id: new ObjectId(req.params.id) })
+    )
+  } catch (ex) {
+    console.log(ex)
+    res.status(400).json({ erro: `${ex}` })
+  }
+})
+
+router.delete('/undproduto/:id', async function (req, res, next) {
+  try {
+    const db = await connect()
+    res.json(
+      await db
+        .collection('undproduto')
+        .deleteOne({ _id: new ObjectId(req.params.id) })
+    )
+  } catch (ex) {
+    console.log(ex)
+    res.status(400).json({ erro: `${ex}` })
+  }
+})
+
 
 app.listen(port)
 
